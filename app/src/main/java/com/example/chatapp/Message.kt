68bdfbebaec47a7b9292911453f72bea4262b5f9
Argam <mehrabyanargam111@gmail.com>
2023-01-25 12:52:35 +0400

    created basic design

---
+++ app/src/main/java/com/example/chatapp/Message.kt
@@ -0,0 +1,41 @@
+package com.example.chatapp
+
+import java.util.*
+
+class Message {
+    var name: String = ""
+    var textMessage: String = ""
+    private var messageTime: Long = 0
+
+    constructor() {}
+    constructor(name: String, textMessage: String): this() {
+        this.name = name
+        this.textMessage = textMessage
+        this.messageTime = Date().time
+    }
+
+    fun getUsername() : String{
+        return name
+    }
+
+    fun setUsername(userName: String) {
+        this.name = userName
+    }
+
+    fun getTextsMessage() : String{
+        return textMessage
+    }
+
+    fun setTextsMessage(textMessage: String) {
+        this.textMessage = textMessage
+    }
+
+    fun getMessageTime() : Long{
+        return messageTime
+    }
+
+    fun setMessageTime(textMessage: Long) {
+        this.messageTime = messageTime
+    }
+
+}
